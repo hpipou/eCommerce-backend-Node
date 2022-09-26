80f4f4706d1808e2f05cbcfaf68ba2563307900a
hpipou <mr.aoudjit@gmail.com>
2022-09-26 20:23:46 +0200

    NEXT TICKET -- ORDER

--- routes/order.js
+++ routes/order.js
@@ -1,6 +1,13 @@
 const express = require("express")
 const orderRoutes= express.Router()
+const models = require("../models")
+const jwt = require("jsonwebtoken")
+const validator = require("validator")
 
+// génerer une facture
 
+// afficher toutes les factures
+
+// afficher une seule facture
 
 module.exports=orderRoutes
